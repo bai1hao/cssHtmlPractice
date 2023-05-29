2751ea66a7daface3a1dfa2c1a95cd783b3f6162
bai1hao <2360798309@qq.com>
2023-05-29 15:06:13 +0800

    c4完成

--- partC/c4/index.js
+++ partC/c4/index.js
@@ -1,13 +1,17 @@
 //Here is your CODE!
-let $=e=>document.querySelector(e)
-let $$=e=>document.querySelectorAll(e)
-window.onload=e=>{
-    console.log($$('.container'));
-    $('.render-btn').addEventListener('click',render)
+let $ = e => document.querySelector(e)
+let $$ = e => document.querySelectorAll(e)
+window.onload = e => {
+    $('.render-btn').addEventListener('click', render)
 }
-var render = (e) => {
-    if(window.getSelection().rangeCount!==0){
-        console.log('wewr')
-    }
 
+let render = (e) => {
+    if (window.getSelection().rangeCount !== 0) {
+        let selection = window.getSelection();
+        let ranges = selection.getRangeAt(0)
+        let el = document.createElement('span');
+        el.style.background='yellow';
+        el.style.color='red';
+        ranges.surroundContents(el);
+    }
 };
